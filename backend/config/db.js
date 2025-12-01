292cc66b563813a823623550a88d9af5d7f0cdaf
tamal006 <tamalkumarkhan006@gmail.com>
2025-12-01 15:01:41 +0530

    Update MongoDB connection string to use environment variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://tamalkumarkhan006_db_user:gnigGBI7iWCKgAQ6@cluster0.pswxhtz.mongodb.net/?appName=Cluster0')
+    await mongoose.connect('process.env.DP_URL')
     .then(() => {console.log("MongoDB connected successfully");
     });
   } catch (error) {
